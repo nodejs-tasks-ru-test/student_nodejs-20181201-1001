8f06f2f222b03ca1419657eec7aa211268a477d9
Sergey Zelenov <github@zelenov.su>
2019-01-01 12:23:40 +0100

    Added task Сумма двух чисел

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,9 +1,5 @@
 function sum(a, b) {
-  if ([a, b].some(value => typeof value !== 'number')) {
-    throw new TypeError();
-  }
-  
-  return a + b;
+  /* ваш код здесь */
 }
 
 module.exports = sum;
